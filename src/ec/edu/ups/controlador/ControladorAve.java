f743d4aec138734113ecb700ffa50f3649cde669
Darwin <Darwin@DESKTOP-5O0JVM5>
2019-04-25 02:18:29 -0500

    Creacion de clase hija 3 con atributos, constructores y metodos; Se crea clase ConstructorAve en la cual se implemeta el CRUD y ademas los objetos creados se guardan en una interfaz TreeSet

--- src/ec/edu/ups/controlador/ControladorAve.java
+++ src/ec/edu/ups/controlador/ControladorAve.java
@@ -5,10 +5,84 @@
  */
 package ec.edu.ups.controlador;
 
+import ec.edu.ups.modelo.Ave;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
  * Clase  ControladorAve debe ser implementada usando la interface SortedSet y la clase TreeSet.
  * @author Darwin Leon, Est.
  */
 public class ControladorAve {
+    
+     //Interfaz SortedSet guarda lista de objetos en un TreeSet el cual permite el ingreso
+    //de un objeto con un unico codigo. Aqui se genera el codigo automaticamente.
+    private SortedSet<Ave> listaAve;
+    private int cod;
+    
+   
+    public ControladorAve(){
+        listaAve = new TreeSet<>();
+        cod = 1; 
+}
+    
+    public void create(Ave ave) {
+        ave.setCodigo(cod);
+        listaAve.add(ave);
+        cod++;
+    }
+    
+     /**
+     * Metodo read de la clase ControladorAve .
+     *
+     * @param codigo que es ingresado desde el teclado para la lectura.
+     * @return el objeto ave.
+     */
+    public Ave read(int codigo) {
+        for (Ave aveR : listaAve) {
+            if (aveR.getCodigo()==codigo) {
+                return aveR;
+            }else{
+                if(aveR.getCodigo()!=codigo){
+                    System.out.println(" El codigo del ave no existe o ya fue eliminado");
+                }
+            }
+        } 
+        return null;
+    }
+    
+    public Ave update(Ave aveU){
+            if (listaAve.contains(aveU)) { 
+                listaAve.remove(aveU);
+                aveU.setCodigo(cod); ///implemente esto para ver si sale el mismo codigo al momento de actualizar
+                listaAve.add(aveU); 
+            }
+        return null;
+    }
+    
+     /**
+     *
+     * @param codigo 
+     */
+    public void delete(Ave ave) {
+        for (Ave aveD : listaAve) {
+            if (aveD.equals(ave)) {
+                listaAve.remove(aveD);
+                break;
+            }  
+        }    
+    }
+    
+   
+    public void imprimir(){
+        for (Ave aveI : listaAve) {
+            //imprime todo el objeto
+            System.out.println(aveI);
+        }
+    }
+    
+    
+    
+    
     
 }
---
+++ src/ec/edu/ups/modelo/Ave.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+/**
+ *
+ * @author Darwin Leon, Est.
+ */
+public class Ave extends Animal {
+    
+    //atributos privados de la clase Ave.
+    private String colorPlumaje;
+    private String tipoAve;
+    private int tamaño;
+    private String tiempoGest;
+    
+    /**
+     * Constrcutor vacio.
+     */
+    public Ave(){ 
+    }
+
+    /**
+     * Constructor con todos los paremetros de la clase y los que fueron heredados.
+     * @param colorPlumaje
+     * @param tipoAve
+     * @param tamaño
+     * @param tiempoGest
+     * @param codigo
+     * @param nombre
+     * @param tipoAnimal
+     * @param sexo 
+     */
+    public Ave(String colorPlumaje, String tipoAve, int tamaño, String tiempoGest, int codigo, String nombre, String tipoAnimal, String sexo) {
+        super(codigo, nombre, tipoAnimal, sexo);
+        this.colorPlumaje = colorPlumaje;
+        this.tipoAve = tipoAve;
+        this.tamaño = tamaño;
+        this.tiempoGest = tiempoGest;
+    }
+
+    //METODOS SETTER'S Y GETTER'S.
+    public String getColorPlumaje() {
+        return colorPlumaje;
+    }
+
+    public void setColorPlumaje(String colorPlumaje) {
+        this.colorPlumaje = colorPlumaje;
+    }
+
+    public String getTipoAve() {
+        return tipoAve;
+    }
+
+    public void setTipoAve(String tipoAve) {
+        this.tipoAve = tipoAve;
+    }
+
+    public int getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(int tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getTiempoGest() {
+        return tiempoGest;
+    }
+
+    public void setTiempoGest(String tiempoGest) {
+        this.tiempoGest = tiempoGest;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "color de Plumaje= " + colorPlumaje +"\n"+ 
+                "tipo de Ave=" + tipoAve + ", tamaño= " + tamaño + ", tiempo de Gestacion= " + tiempoGest;
+    }
+
+    @Override
+    public int compareTo(Animal o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+   
+}
